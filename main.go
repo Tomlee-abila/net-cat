34a4096b10c7878e53c995ec549bafef61c1f621
Tomlee Abila <tomleeabila@gmail.com>
2025-01-15 17:03:57 +0300

    feat: create server and clients for the chat

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"time"
 )
 
 type Message struct {
 	from    string
+	name    string
 	payload []byte
+}
+
+type Client struct {
+	conn  net.Conn
+	ipAdd string
 }
 
 type Server struct {
@@ -16,6 +24,7 @@ type Server struct {
 	ln         net.Listener
 	quitch     chan struct{}
 	msgch      chan Message
+	clients    []Client
 }
 
 func NewServer(listenAddr string) *Server {
@@ -51,28 +60,36 @@ func (s *Server) acceptLoop() {
 			continue
 		}
 
-		fmt.Println("New connection to the Server:", conn.RemoteAddr())
-		go s.readLoop(conn)
+		conn.Write([]byte("Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]:"))
+		buf := make([]byte, 2048)
+		n, err := conn.Read(buf)
+
+		name := string(buf[:n-1])
+
+		fmt.Println("New connection to the Server:", name, conn.RemoteAddr())
+		go s.readLoop(conn, name)
 	}
 }
 
-func (s *Server) readLoop(conn net.Conn) {
+func (s *Server) readLoop(conn net.Conn, name string) {
 	defer conn.Close()
+
 	buf := make([]byte, 2048)
 
 	for {
-		n, err := conn.Read(buf)
+		_, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println("read error:", err)
-			continue
+			fmt.Println("Lee has left our chat...")
+			return
 		}
 
 		s.msgch <- Message{
 			from:    conn.RemoteAddr().String(),
-			payload: buf[:n],
+			payload: buf,
+			name:    name,
 		}
 
-		conn.Write([]byte("Thank you for your message!"))
+		// conn.Write([]byte("Thank you for your message!"))
 
 		// msg := buf[:n]
 		// fmt.Println(string(msg))
@@ -81,15 +98,28 @@ func (s *Server) readLoop(conn net.Conn) {
 }
 
 func main() {
+	if len(os.Args) > 2 {
+		fmt.Println("[USAGE]: ./TCPChat $port")
+	}
+	port := "8989"
 
-	
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
 
-	server := NewServer(":3000")
+	server := NewServer(":" + port)
 
 	go func() {
 		for msg := range server.msgch {
-			fmt.Printf("received message from connection(%s): %s", msg.from, string(msg.payload))
+			t := time.Now()
+			fmt.Printf("[%s][%s]:%s", t.Format("02-01-2006 15:04:05"), msg.name, string(msg.payload))
 		}
 	}()
-	log.Fatal(server.Start())
+	if err := server.Start(); err != nil {
+		// fmt.Println("err:", err)
+		port = "8989"
+		server = NewServer(":" + port)
+		log.Fatal(server.Start())
+	}
+	fmt.Printf("Listening on the port :%s\n", port)
 }
